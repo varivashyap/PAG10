ede4fcae147744fae007ec577119831e47385189
AnishkaVaitla <119092759+AnishkaVaitla@users.noreply.github.com>
2024-11-22 19:17:20 +0530

    Create main.py

---
+++ Bird's Eye View/main.py
@@ -0,0 +1,13 @@
+from detect import run, parse_opt
+from utils.general import check_requirements
+from pathlib import Path
+
+def birdEyeView(input_video):
+    opt = parse_opt()
+    opt.source = input_video
+    check_requirements(exclude=('tensorboard', 'thop'))
+    run(**vars(opt))
+    return Path("video.mp4")
+
+input_video = "/home/urvashi2022/Documents/Soccer-Player-Tracking-YoloV5/birdEyeView/data/videos/demo-s.mp4"
+output_video = birdEyeView(input_video)
